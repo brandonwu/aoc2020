8173d0b6e5d4c12edfdfa5fedd68a0c9fc211dcd
brandon.wu <brandon.wu@workday.com>
2020-12-07 12:19:52 -0800

    day 5 const mem

--- 5/1.py
+++ 5/1.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
+
 def parse(line):
-    return line.replace('F', '0').replace('B', '1') \
-        .replace('L', '0').replace('R', '1')
+    n = line.replace('F', '0').replace('B', '1') \
+        .replace('L', '0').replace('R', '1').strip()
+    return int(n, 2)
 
 
 def main(arr):
-    return max([int(n, 2) for n in arr])
+    cur_max = -1
+
+    for line in f:
+        n = parse(line)
+        if n > cur_max:
+            cur_max = n
+
+    return cur_max
 
 
 if __name__ == '__main__':
     with open('input', 'r') as f:
-        arr = f.readlines()
-        print(main([parse(line.strip()) for line in arr]))
+        print(main(f))
--- 5/2.py
+++ 5/2.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
 
+
 def parse(line):
-    return line.replace('F', '0').replace('B', '1') \
-        .replace('L', '0').replace('R', '1')
+    n = line.replace('F', '0').replace('B', '1') \
+        .replace('L', '0').replace('R', '1').strip()
+    return int(n, 2)
+
+
+def gauss(n):
+    return n * (n + 1) / 2
 
 
 def main(arr):
-    nums = [int(n, 2) for n in arr]
-    r = set(range(min(nums), max(nums) + 1))
+    total = 0
+    cur_min = float('inf')
+    cur_max = -1
 
-    for n in nums:
-        r.remove(n)
+    for line in f:
+        n = parse(line)
+        if n < cur_min:
+            cur_min = n
+        if n > cur_max:
+            cur_max = n
+        total += n
 
-    for n in r:
-        return n
+    return gauss(cur_max) - gauss(cur_min - 1) - total
 
 
 if __name__ == '__main__':
     with open('input', 'r') as f:
-        arr = f.readlines()
-        print(main([parse(line.strip()) for line in arr]))
+        print(main(f))
