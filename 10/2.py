22494f55f6d0408adb603d95d87b934852f7dd85
brandon.wu <brandon.wu@workday.com>
2020-12-13 21:21:24 -0800

    day 10 p2 memo soln

--- 10/2.py
+++ 10/2.py
@@ -12,27 +12,31 @@ def parse(line):
     return int(line)
 
 
-def dfs(node, graph):
+def dfs(node, graph, memo):
     val, pos = node
     # base case
     if pos == (len(graph) - 1):
         return 1
     # recurse
+    elif pos in memo:
+        return memo[pos]
     else:
         acc = 0
         for i in range(pos + 1, min(pos + 4, len(graph))):
             testval = graph[i]
 
             if testval - val <= 3:
-                ways = dfs((testval, i), graph)
-
+                ways = dfs((testval, i), graph, memo)
                 acc += ways
+
+                if i not in memo:
+                    memo[i] = ways
 
         return acc
 
 
 def dfs_help(graph):
-    return dfs((0, 0), graph)
+    return dfs((0, 0), graph, {})
 
 
 def main(arr):
